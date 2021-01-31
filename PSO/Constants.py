0b95fef4fee14474f29a649e10ac57be938160d3
ghis9 <ghis9917@gmail.com>
2021-01-31 19:17:32 +0100

    Minor changes, added graph of altitude values reached with every iteration

--- PSO/Constants.py
+++ PSO/Constants.py
@@ -1,12 +1,12 @@
 # ---PARTICLE consts
-MAX_POS = 1000
+MAX_POS = 10000
 MIN_POS = 0
 MAX_VEL = 1
 MIN_VEL = 0.1
 W = 0.5
-C1 = 1
-C2 = 1
+C1 = 2
+C2 = 2
 
 # ---PSO consts
-N_PARTICLES = 5
+N_PARTICLES = 2
 N_ITERATIONS = 500
--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from PSO.ParticleSwarmOptimization import PSO
 from PSO.Position import Position
 
 
-def function(position: Position) -> float:
-    return np.sqrt(
-        position.x ** 2 +
-        position.y ** 2
-    ) / 10
+def function(position: Position):
+    return -( (12 * np.cos((position.x**2 + position.y**2)/4)) / (3 + position.x**2 + position.y**2))
 
 
 my_pso = PSO(function)
 history = my_pso.optimize()
-
+print([i[1] for i in history])
 plt.plot([i for i in range(len(history))], [i[1] for i in history])
 plt.show()
