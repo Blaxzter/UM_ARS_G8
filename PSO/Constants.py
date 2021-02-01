c5172f20543cc7a05124dbed50adf570df3b2234
ghis9 <ghis9917@gmail.com>
2021-02-01 01:29:39 +0100

    Updated README and prettified the Constants file

--- PSO/Constants.py
+++ PSO/Constants.py
@@ -1,12 +1,12 @@
 # ---PARTICLE consts
-MAX_POS = 25        # Maximum value of the position for both X and Y throughout the whole simulation
-MIN_POS = -25       # Minimum value of the position for both X and Y throughout the whole simulation
-MAX_VEL = 1         # Maximum value of the velocity for both X and Y for the initialization
-MIN_VEL = 0.1       # Minimum value of the velocity for both X and Y for the initialization
-W = 0.2             # Factor for the current velocity (> means exploitation, < means exploration)
-C1 = 2              # Factor for the cognitive velocity (>C2 means exploitation, <C2 means exploration)
-C2 = 2              # Factor for the cognitive velocity (>C1 means exploration, <C1 means exploitation)
+MAX_POS = 25            # Maximum value of the position for both X and Y throughout the whole simulation
+MIN_POS = -25           # Minimum value of the position for both X and Y throughout the whole simulation
+MAX_VEL = 1             # Maximum value of the velocity for both X and Y for the initialization
+MIN_VEL = 0.1           # Minimum value of the velocity for both X and Y for the initialization
+W = 0.2                 # Factor for the current velocity (> means exploitation, < means exploration)
+C1 = 2                  # Factor for the cognitive velocity (>C2 means exploitation, <C2 means exploration)
+C2 = 2                  # Factor for the cognitive velocity (>C1 means exploration, <C1 means exploitation)
 
 # ---PSO consts
-N_PARTICLES = 50    # Number of particles used for the simulation
-N_ITERATIONS = 1000 # Number of iterations used for the PSO
+N_PARTICLES = 500       # Number of particles used for the simulation
+N_ITERATIONS = 1000     # Number of iterations used for the PSO
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Particle Swarm Optimization
 
-Python implenetation of the PSO algorithm
+Python implementation of the PSO algorithm for 2D functions
+
+- The First graph plotted shows the optimization process animated with the particles reaching the minima found over the 
+contour of the function to minimize
+- The Second graph shows the behaviour of the particles throughout the optimization process by plotting the altitude 
+  reached in every iteration. Such behaviour drops to the local (or global) minima found by the PSO algorithm
+
