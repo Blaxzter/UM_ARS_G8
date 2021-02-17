Fixed crossover but average fitness doesn't seem to get better over time

03_Genetic_Algorithm/src/Constants.py
@@ -4,7 +4,7 @@ import pygame
 environment_speed = 0.1
 
 padding = 20
-padding_top = 20
+padding_top = 70
 
 width = 1100
 height = 700
@@ -42,3 +42,4 @@ start_location = np.array([start_x, start_y], dtype=float).reshape((2, 1))
 goal = np.array([width - 100, 100], dtype=float).reshape((2, 1))
 
 individuals_life_steps = 500
+elitism_rate = 3

03_Genetic_Algorithm/src/Genome.py
@@ -9,17 +9,19 @@ class Genome:
     def __init__(self):
         self.genes: List[Dict[float, float]] = self.init_genome()
 
-    def crossover(self, other):
-        one_point = randint(0, Const.individuals_life_steps)
+    def crossover(self, partner):
+        first_point = randint(Const.individuals_life_steps // 2, Const.individuals_life_steps)
+        new_genes = []
         for i in range(Const.individuals_life_steps):
-            if i < one_point:
-                temp = self.genes[i]
-                self.genes[i] = other.genes[i]
-                other.genes[i] = temp
+            if first_point < i:
+                new_genes.append(partner.genes[i])
+            else:
+                new_genes.append(self.genes[i])
+        return new_genes
 
     def mutation(self):
         for i in range(Const.individuals_life_steps):
-            if np.random.uniform(low=0, high=1) < 0.5:
+            if np.random.uniform(low=0, high=1) < 0.08:
                 self.genes[i] = {
                     'd_v_l': np.random.choice([-1, 0, 1]) * Const.robot_velocity_steps,
                     'd_v_r': np.random.choice([-1, 0, 1]) * Const.robot_velocity_steps