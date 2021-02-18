152baa1a7ebbe4f987a3b26147f4013b386c43b2
ghis9 <ghis9917@gmail.com>
2021-02-18 17:12:28 +0100

    seaborn theme

--- 03_Genetic_Algorithm/src/main.py
+++ 03_Genetic_Algorithm/src/main.py
@@ -1,9 +1,11 @@
 from Simulator import Simulator
+import seaborn as sns
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     sim = Simulator()
     sim.start()
+    sns.set_theme()
     plt.plot(
         [i for i in range(len(sim.population.avg_fitness))],
         sim.population.avg_fitness,
